216ea14c5175f08301ce3288978a455b23e9f598
kajedevid <stokakunnnnn23@gmail.com>
2024-11-20 13:13:21 +0800

    Add files via upload

---
+++ plugins/sc.js
@@ -0,0 +1,11 @@
+export const cmd = {
+    name: ['base'],
+    command: ['base'],
+    category: ['main'],
+    detail: {
+        desc: 'Informasi skrip bot ini'
+    },
+    async start({ m }) {
+            m.reply('Bot Ini menggunakan base: https://github.com/kazedepid/whatsapp-bot')
+    }
+}
